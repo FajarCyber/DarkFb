3a4c0f3dcf84f522ca26ef2716ca96abd576b7ff
TimeEror123 <67280289+TimeEror123@users.noreply.github.com>
2020-07-01 05:08:25 +0700

    Create Dark.sh

---
+++ Dark.sh
@@ -0,0 +1,72 @@
+#Ngapain lu Gblk
+#Mau ngerecode yh
+#bngst kau
+
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+lightgreen='\e[1;32m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+
+figlet -f slant "DARK $red FB"             
+echo
+echo "$blue [»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»]"
+echo "   $blue [                  $red AUTHOR : TIME EROR              $blue ]"
+echo "   $blue [           $red FACEBOOK  : ?????????? ???????????     $blue ]"
+echo "   $blue [            $red GITHUB : HTTPS://GITHUB.COM/TIME EROR $blue ]"
+echo "   $blue [                  $red SCRIPT : BY TIME EROR           $blue ]"
+echo "$blue [»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»]"
+echo "$purple SILAKAN LOGIN FB "
+echo
+read -p "USERNAME/EMAIL : " read tr
+sleep 1
+read -p "PASSWORD :" read tr
+echo "$yellow SEDANG MASUK..."
+sleep 1
+sleep 1
+sleep 1
+sleep 1                                                         sleep 1
+sleep 1
+echo
+echo "$green [✓]LOGIN BERHASIL ✓"
+read -p "[KEMBALI]" read tr
+
+clear
+#Ngapain lu Gblk
+#Mau ngerecode yh
+#bngst kau
+
+clear
+blue='\033[34;1m'
+lightblue='\e[1;34m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+lightgreen='\e[1;32m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+
+cowsay -f ghostbusters "W E L C O M E ! ! !"|lolcat             
+echo
+echo "$blue [»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»]"
+echo "   $blue [                  $red AUTHOR : TIME EROR              $blue ]"
+echo "   $blue [           $red FACEBOOK  : ?????????? ???????????     $blue ]"
+echo "   $blue [            $red GITHUB : HTTPS://GITHUB.COM/TIME EROR $blue ]"
+echo "   $blue [                  $red SCRIPT : BY TIME EROR           $blue ]"
+echo "$blue [»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»]"
+echo "$lightblue SILAKAN PILIH MENU HACK FB"
+echo
+echo "\e[1;34m SILAKAN PILIH MENU HACK FB"
+
+echo "\e[1;34m 1.CRACK DARI TEMAN [TARGET]"
+echo "\e[1;34m 2.CRACK DARI GRUPP"
+echo "\e[1;34m 3.BRUTEFORCE"
+echo "\e[1;34m 4.MULTI BRUTEFORCE [TARGET]"
+echo
+read -p "PILIH : " read tr
+
